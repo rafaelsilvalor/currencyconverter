38b9050300bd6d97e9915f69227baca42fa7784e
Rafael Silva <rafaelgslor@gmail.com>
2025-04-23 16:51:32 -0300

    Add CommandLineParser to support direct CLI conversion commands

---
+++ src/main/java/com/rafellor/currencyconverter/cli/CommandLineParser.java
@@ -0,0 +1,18 @@
+package com.rafellor.currencyconverter.cli;
+
+import java.util.function.DoubleToLongFunction;
+
+public class CommandLineParser {
+
+    public record Command(String from, String to, double amount) {}
+
+    public static Command parse(String[] args) {
+        if ( args.length == 4 && args[0].equalsIgnoreCase("cc")) {
+            String from = args[1].toUpperCase();
+            String to = args[2].toUpperCase();
+            double amount = Double.parseDouble(args[3]);
+            return new Command(from, to ,amount);
+        }
+        throw  new IllegalArgumentException("Invalid command. Format: cc USD BRL 50.0");
+    }
+}
